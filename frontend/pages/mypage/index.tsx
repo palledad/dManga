de0d7dd95b72a8f536ae7da093bd441ca229671c
yooto <yuto174@icloud.com>
2023-02-19 20:39:50 +0900

    mypageを追加

--- frontend/pages/mypage/index.tsx
+++ frontend/pages/mypage/index.tsx
@@ -1,5 +1,21 @@
-export default function mypage() {
+import Segment from '../../components/Segment'
+import Header from '../../components/Header'
+import { Box, Grid, GridItem } from '@chakra-ui/react'
+
+const MyPage = () => {
   return (
-    <></>
+    <>
+      <Header></Header>
+      <Box height={'20em'} background={'gray.100'}></Box>
+      <Segment>
+        <Grid templateColumns='repeat(5, 1fr)' templateRows='repeat(2, 1fr)'>
+          <GridItem colSpan={2}>ユーザー名</GridItem>
+          <GridItem colSpan={3}>wallet address</GridItem>
+          <GridItem colSpan={2}>自己紹介文</GridItem>
+        </Grid>
+      </Segment>
+    </>
   )
 }
+
+export default MyPage
