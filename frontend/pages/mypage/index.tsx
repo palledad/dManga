b9ead7dd809a7383637dba198d3ddb88ddfe3aa7
yooto <yuto174@icloud.com>
2023-02-19 21:04:39 +0900

    mypageにtabを追加

--- frontend/pages/mypage/index.tsx
+++ frontend/pages/mypage/index.tsx
@@ -1,6 +1,17 @@
 import Segment from '../../components/Segment'
 import Header from '../../components/Header'
-import { Box, Grid, GridItem } from '@chakra-ui/react'
+import {
+  Box,
+  Grid,
+  GridItem,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Button,
+  Input,
+} from '@chakra-ui/react'
 
 const MyPage = () => {
   return (
@@ -13,6 +24,55 @@ const MyPage = () => {
           <GridItem colSpan={3}>wallet address</GridItem>
           <GridItem colSpan={2}>自己紹介文</GridItem>
         </Grid>
+      </Segment>
+      <Segment>
+        <Tabs variant='enclosed'>
+          <TabList>
+            <Tab>購入した記事</Tab>
+            <Tab>自分の記事</Tab>
+            <Tab>持っている収益NFT</Tab>
+          </TabList>
+
+          <TabPanels>
+            <TabPanel>
+              <Input placeholder='検索' background={'gray.100'} />
+              <Segment>
+                記事サムネイル画像
+                <br />
+                タイトル
+                <br />
+                作者
+                <br />
+                作成日時
+                <br />
+                更新日時
+              </Segment>
+            </TabPanel>
+            <TabPanel>
+              <Input placeholder='検索' background={'gray.100'} />
+              <Segment>
+                タイトル
+                <br />
+                作成日時
+                <br />
+                更新日時
+                <br />
+                <Button>編集</Button>
+                <Button>削除</Button>
+              </Segment>
+            </TabPanel>
+            <TabPanel>
+              <Input placeholder='検索' background={'gray.100'} />
+              <Segment>
+                記事URL
+                <br />
+                記事タイトル
+                <br />
+                トークンID
+              </Segment>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
       </Segment>
     </>
   )
