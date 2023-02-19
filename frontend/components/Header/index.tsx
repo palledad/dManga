3406225979868f9f236136563e61358b52d76341
yooto <yuto174@icloud.com>
2023-02-19 20:40:18 +0900

    headerを修正

--- frontend/components/Header/index.tsx
+++ frontend/components/Header/index.tsx
@@ -2,7 +2,7 @@ import { Image, GridItem, Button, Grid } from '@chakra-ui/react'
 const Header = () => {
   return (
     <>
-      <Grid templateColumns='repeat(5, 1fr)' background='gray.500' height={45}>
+      <Grid templateColumns='repeat(5, 1fr)' background={'gray.500'} height={'3em'}>
         <GridItem colSpan={1}>
           <Image marginLeft={1} borderRadius='full' boxSize='30px' src='palledad_logo_1.jpg' />
         </GridItem>
