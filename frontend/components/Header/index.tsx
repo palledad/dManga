2d4004979e9720a00356af670ebdac911b0cc4f8
yooto <yuto174@icloud.com>
2023-02-19 20:16:18 +0900

    headerを追加

---
+++ frontend/components/Header/index.tsx
@@ -0,0 +1,25 @@
+import { Image, GridItem, Button, Grid } from '@chakra-ui/react'
+const Header = () => {
+  return (
+    <>
+      <Grid templateColumns='repeat(5, 1fr)' background='gray.500' height={45}>
+        <GridItem colSpan={1}>
+          <Image marginLeft={1} borderRadius='full' boxSize='30px' src='palledad_logo_1.jpg' />
+        </GridItem>
+        <GridItem colSpan={4} textAlign='right'>
+          <Button size='sm' verticalAlign='middle' marginX={1}>
+            記事検索
+          </Button>
+          <Button size='sm' marginX={1}>
+            記事作成
+          </Button>
+          <Button size='sm' marginX={1}>
+            アカウント
+          </Button>
+        </GridItem>
+      </Grid>
+    </>
+  )
+}
+
+export default Header
