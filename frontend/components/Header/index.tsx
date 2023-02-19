aaac8dd9c73a8086071d5c0e8c8d83abd32af425
yooto <yuto174@icloud.com>
2023-02-19 22:24:59 +0900

    headerのレイアウトを修正

--- frontend/components/Header/index.tsx
+++ frontend/components/Header/index.tsx
@@ -1,24 +1,16 @@
-import { Image, GridItem, Button, Grid } from '@chakra-ui/react'
+import { Image, Box, Button } from '@chakra-ui/react'
 const Header = () => {
   return (
-    <>
-      <Grid templateColumns='repeat(5, 1fr)' background={'gray.500'} height={'3em'}>
-        <GridItem colSpan={1}>
-          <Image marginLeft={1} borderRadius='full' boxSize='30px' src='palledad_logo_1.jpg' />
-        </GridItem>
-        <GridItem colSpan={4} textAlign='right'>
-          <Button size='sm' verticalAlign='middle' marginX={1}>
-            記事検索
-          </Button>
-          <Button size='sm' marginX={1}>
-            記事作成
-          </Button>
-          <Button size='sm' marginX={1}>
-            アカウント
-          </Button>
-        </GridItem>
-      </Grid>
-    </>
+    <Box display='flex' alignItems='center' justifyContent='space-between' h={45} mx={4}>
+      <Box display='flex'>
+        <Image marginLeft={1} borderRadius='full' boxSize='30px' src='palledad_logo_1.jpg' />
+      </Box>
+      <Box display='flex' gap={4}>
+        <Button size='sm'>記事検索</Button>
+        <Button size='sm'>記事作成</Button>
+        <Button size='sm'>アカウント</Button>
+      </Box>
+    </Box>
   )
 }
 
