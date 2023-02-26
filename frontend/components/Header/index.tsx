e298e366df75ce459c6a2f399a8376eee650b7d2
yooto <yuto.ikhr@gmail.com>
2023-02-26 18:40:13 +0900

    ヘッダーのボタンを押すとページ遷移するように変更

--- frontend/components/Header/index.tsx
+++ frontend/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { Image, Box, Button } from '@chakra-ui/react'
+import Link from 'next/link'
 const Header = () => {
   return (
     <Box display='flex' alignItems='center' justifyContent='space-between' h={45} mx={4}>
@@ -6,9 +7,15 @@ const Header = () => {
         <Image marginLeft={1} borderRadius='full' boxSize='30px' src='palledad_logo_1.jpg' />
       </Box>
       <Box display='flex' gap={4}>
-        <Button size='sm'>記事検索</Button>
-        <Button size='sm'>記事作成</Button>
-        <Button size='sm'>アカウント</Button>
+        <Link href='/articlesearch'>
+          <Button size='sm'>記事検索</Button>
+        </Link>
+        <Link href='/createarticle'>
+          <Button size='sm'>記事作成</Button>
+        </Link>
+        <Link href='/mypage'>
+          <Button size='sm'>アカウント</Button>
+        </Link>
       </Box>
     </Box>
   )
