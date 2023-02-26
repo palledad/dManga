761479f1a8e21eaed6d35a5aad05a2e6db645a58
yooto <yuto.ikhr@gmail.com>
2023-02-26 18:51:19 +0900

    トップページにヘッダーを追加・ヘッダーのアイコンからトップページに遷移するように変更

--- frontend/components/Header/index.tsx
+++ frontend/components/Header/index.tsx
@@ -3,9 +3,11 @@ import Link from 'next/link'
 const Header = () => {
   return (
     <Box display='flex' alignItems='center' justifyContent='space-between' h={45} mx={4}>
-      <Box display='flex'>
-        <Image marginLeft={1} borderRadius='full' boxSize='30px' src='palledad_logo_1.jpg' />
-      </Box>
+      <Link href='/'>
+        <Box display='flex'>
+          <Image marginLeft={1} borderRadius='full' boxSize='30px' src='palledad_logo_1.jpg' />
+        </Box>
+      </Link>
       <Box display='flex' gap={4}>
         <Link href='/articlesearch'>
           <Button size='sm'>記事検索</Button>
