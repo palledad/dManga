c67c3c24d12b06c0256ac169aace598ebfec9d1a
Rei Sugiura <reibomaru@gmail.com>
2023-03-05 21:02:19 +0900

    temp

--- backend/internal/models/articles.go
+++ backend/internal/models/articles.go
@@ -1,15 +1,13 @@
 package models
 
 import (
-	"fmt"
-
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type Article struct {
 	gorm.Model
-	ID            uuid.UUID `gorm:"primarykey;type:uuid;default:uuid_generate_v4()"`
+	ID            uuid.UUID
 	Title         string
 	Content       string
 	AuthorAddress string
@@ -23,18 +21,27 @@ func NewArticleModel() *ArticleModel {
 	return &ArticleModel{}
 }
 
-func (a *ArticleModel) ReadArticle(db *gorm.DB, alias string) (*Article, error) {
-	target := &Article{Alias: alias}
-	if result := db.First(&target); result.Error != nil {
+func (m *ArticleModel) CreateArticle(db *gorm.DB, article *Article) (*Article, error) {
+	if result := db.Create(article); result.Error != nil {
+		return nil, result.Error
+	}
+
+	return article, nil
+}
+
+func (m *ArticleModel) ReadArticle(db *gorm.DB, alias string) (*Article, error) {
+	target := &Article{}
+	if result := db.Where("alias = ?", alias).First(&target); result.Error != nil {
 		return nil, result.Error
 	}
 	return target, nil
 }
 
-func (m *ArticleModel) CreateArticle(db *gorm.DB, article *Article) error {
-	fmt.Println(article.ID)
-	if result := db.Create(article); result.Error != nil {
-		return result.Error
+func (m *ArticleModel) UpdateArticle(db *gorm.DB, articleId uuid.UUID, title string, content string) (*Article, error) {
+	target := &Article{ID: articleId}
+	if result := db.Model(target).Updates(Article{Title: title, Content: content}); result.Error != nil {
+		return nil, result.Error
 	}
-	return nil
+	db.First(&target)
+	return target, nil
 }
--- backend/internal/services/articles.go
+++ backend/internal/services/articles.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"github.com/google/uuid"
 	"github.com/palledad/dManga/backend/internal/models"
 	"gorm.io/gorm"
 )
@@ -18,8 +19,31 @@ func NewArticlesService(db *gorm.DB, articleModel *models.ArticleModel) *Article
 }
 
 func (a *ArticleService) CreateArticle(article *models.Article) (*models.Article, error) {
-	if err := a.articleModel.CreateArticle(a.db, article); err != nil {
+	if _, err := a.articleModel.CreateArticle(a.db, article); err != nil {
 		return nil, err
 	}
 	return article, nil
+}
+
+func (a *ArticleService) ReadArticle(alias string) (*models.Article, error) {
+	article, err := a.articleModel.ReadArticle(a.db, alias)
+	if err != nil {
+		return nil, err
+	}
+	return article, nil
+}
+
+func (a *ArticleService) UpdateArticle(articleId uuid.UUID, title string, content string) (*models.Article, error) {
+	tx := a.db.Begin()
+	articleResp, err := a.articleModel.UpdateArticle(tx, articleId, title, content)
+	if err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+	err = tx.Commit().Error
+	if err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+	return articleResp, nil
 }
