f9df5884f5ec256b199ef5d1718f5e482a8881a0
Rei Sugiura <reibomaru@gmail.com>
2023-04-02 20:54:54 +0900

    feat: openapiでgenerateし、関数を準備

---
+++ backend/internal/controller/tags.go
@@ -0,0 +1,23 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+// (POST /tags)
+func (h Handler) PostTag(c *gin.Context) {
+
+}
+
+// (DELETE /tags/{tag_id})
+func (h Handler) DeleteTag(c *gin.Context, tagId string) {
+
+}
+
+// (GET /tags/{tag_id})
+func (h Handler) GetTag(c *gin.Context, tagId string) {
+
+}
+
+// (PATCH /tags/{tag_id})
+func (h Handler) UpdateTag(c *gin.Context, tagId string) {
+
+}
