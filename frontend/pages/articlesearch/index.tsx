1c6e3aa4c868c286c7783771c29548842fad4383
yooto <yuto.ikhr@gmail.com>
2023-04-23 17:43:04 +0900

    記事をgetする処理を追加

--- frontend/pages/articlesearch/index.tsx
+++ frontend/pages/articlesearch/index.tsx
@@ -3,8 +3,33 @@ import Header from '../../components/Header'
 import { Grid, GridItem, Input } from '@chakra-ui/react'
 import SearchArticleCard from '../../components/Card/SearchArticleCard'
 import Link from 'next/link'
-
+import { useEffect } from 'react'
 const MyPage = () => {
+  useEffect(() => {
+    console.log('useEffect')
+    const articleAlias = ['string', '2', '3', 'eeee', 'aaa', 'onvm8flrpr']
+
+    const headers = {
+      accept: 'application/json',
+    }
+
+    const fetchArticle = async (alias: string) => {
+      await fetch(`http://localhost:3000/api/articles/${alias}`, {
+        method: 'GET',
+        headers: headers,
+      }).then((response) => {
+        if (!response.ok) {
+          console.log('error', response)
+        } else {
+          console.log('ok', response)
+        }
+      })
+    }
+
+    for (let i = 0; i < articleAlias.length; i++) {
+      fetchArticle(articleAlias[i])
+    }
+  }, [])
   return (
     <>
       <Header></Header>
