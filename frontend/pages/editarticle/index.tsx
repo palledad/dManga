9753122621f4fcf7ead793fceb6a1cbbe1bd5ada
yooto <yuto.ikhr@gmail.com>
2023-04-23 13:33:46 +0900

    記事をPOSTする機能を追加

--- frontend/pages/editarticle/index.tsx
+++ frontend/pages/editarticle/index.tsx
@@ -1,6 +1,6 @@
 import Header from '../../components/Header'
 import Segment from '../../components/Segment'
-import { Box } from '@chakra-ui/react'
+import { Box, Button, FormLabel, Input, FormControl } from '@chakra-ui/react'
 import dynamic from 'next/dynamic'
 import { useEffect, useState } from 'react'
 const CustomEditor = dynamic(() => import('../../components/EditorJS/Editor'), {
@@ -9,6 +9,31 @@ const CustomEditor = dynamic(() => import('../../components/EditorJS/Editor'), {
 
 const EditArticle = () => {
   const [data, setData] = useState<any>()
+  const [alias, setAlias] = useState('')
+  const [title, setTitle] = useState('')
+
+  const postArticle = () => {
+    const headers = {
+      accept: 'application/json',
+      'Content-Type': 'application/json',
+    }
+    fetch('http://localhost:3000/api/articles', {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify({
+        title,
+        content: JSON.stringify(data),
+        author_address: 'authorAddress',
+        alias,
+      }),
+    }).then((response) => {
+      if (!response.ok) {
+        console.log('error', response.body)
+      } else {
+        console.log('ok', response.body)
+      }
+    })
+  }
 
   useEffect(() => {
     console.log(JSON.stringify(data))
@@ -21,6 +46,15 @@ const EditArticle = () => {
         <Box>記事作成画面</Box>
       </Segment>
       <Segment>
+        <Button onClick={postArticle}>投稿</Button>
+        <FormControl>
+          <FormLabel>Alias</FormLabel>
+          <Input value={alias} onChange={(e: any) => setAlias(e.target.value)} />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Title</FormLabel>
+          <Input value={title} onChange={(e: any) => setTitle(e.target.value)} />
+        </FormControl>
         <Box bg='gray.500' w='100%' p={4}>
           <CustomEditor data={data} onChange={setData} holder='editorjs-container' />
         </Box>
