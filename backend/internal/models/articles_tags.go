5433741a0635612fbfdf02e05113446d5df01143
Rei Sugiura <reibomaru@gmail.com>
2023-05-28 20:59:39 +0900

    feat: articles_tagsのモデル部分作成

---
+++ backend/internal/models/articles_tags.go
@@ -0,0 +1,44 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type ArticleTag struct {
+	gorm.Model
+	ArticleID uuid.UUID
+	TagID     uuid.UUID
+}
+
+type ArticlesTagsModel struct {
+}
+
+func NewArticlesTags() *ArticlesTagsModel {
+	return &ArticlesTagsModel{}
+}
+
+func (m *ArticlesTagsModel) RecordTags(db *gorm.DB, articleID uuid.UUID, tagIDs []uuid.UUID) error {
+	recordingTags := make([]ArticleTag, len(tagIDs))
+	for i, tagID := range tagIDs {
+		articleTag := ArticleTag{
+			ArticleID: articleID,
+			TagID:     tagID,
+		}
+		recordingTags[i] = articleTag
+	}
+	return db.Create(recordingTags).Error
+}
+
+func (m *ArticlesTagsModel) UpdateTags(db *gorm.DB, articleID uuid.UUID, tagIDs []uuid.UUID) error {
+	db.Delete(&ArticleTag{}, "artilce_id = ?", articleID)
+	recordingTags := make([]ArticleTag, len(tagIDs))
+	for i, tagID := range tagIDs {
+		articleTag := ArticleTag{
+			ArticleID: articleID,
+			TagID:     tagID,
+		}
+		recordingTags[i] = articleTag
+	}
+	return db.Create(recordingTags).Error
+}
